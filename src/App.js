a38a97fef5b670b993cbd7f716cd5836d287f75b
rwmutel <romanmutel@gmail.com>
2023-05-14 16:37:52 +0300

    added router to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import "./App.css";
 import PageLayout from "./PageLayout/PageLayout";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>All Petitions</div>,
+  },
+  {
+    path: "/considered",
+    element: <div>Considered Petitions</div>,
+  },
+  {
+    path: "/answered",
+    element: <div>Answered Petitions</div>,
+  },
+  {
+    path: "/user-petitions",
+    element: "My Petitions",
+  },
+]);
 
 function App() {
-  return <PageLayout>Hello world!</PageLayout>;
+  return (
+    <PageLayout>
+      <RouterProvider router={router} />
+    </PageLayout>
+  );
 }
 
 export default App;
