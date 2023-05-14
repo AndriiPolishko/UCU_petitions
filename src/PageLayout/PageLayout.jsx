33e218df3dbe5fe2b9b3abbb6155377c8fe37922
rwmutel <romanmutel@gmail.com>
2023-05-14 16:20:39 +0300

    added logo to the header

--- src/PageLayout/PageLayout.jsx
+++ src/PageLayout/PageLayout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./PageLayout.css";
 import "../App.css";
+import logo from "../media/ucu_logo.svg";
 
 function PageLayout(props) {
   return (
     <div className="main">
-      <header></header>
+      <header>
+        <img src={logo} alt="logo" className="header-logo" />
+      </header>
       <div className="content">{props.children}</div>
       <footer></footer>
     </div>
