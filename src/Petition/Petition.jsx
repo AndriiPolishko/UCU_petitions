2864bb59f48438b9a0b59bf8c581fb51b5fac447
rwmutel <romanmutel@gmail.com>
2023-05-15 15:36:32 +0300

    replaced dummy Petition with the real one

--- src/Petition/Petition.jsx
+++ src/Petition/Petition.jsx
@@ -1,54 +1,65 @@
-import React from 'react';
-import './Petition.css';
-import person from '../media/person.svg';
-import plus from '../media/plus.svg';
+import React from "react";
+import {Link} from "react-router-dom";
+import "./Petition.css";
+import person from "../media/person.svg";
+import plus from "../media/plus.svg";
 
 const Petition = () => {
-    return (
-        <div className="pet-item">
-            <div className="pet-content">
-                <div className="pet-left">
-                    <div className="pet-top">
-                        <span className="pet-number">№22/192830-еп</span>
-                        <span className="pet-separator">|</span>
-                        <span className="pet-tag">#Без теми</span>
-                    </div>
-                    <div className="pet-title">
-                        <a className="pet-link" href="/petition/192830">
-                            ТУТ МАЄ БУТИ ЗАГОЛОВОК.
-                        </a>
-                    </div>
-                    <div className="pet-hide">
-                        <div className="pet-date-container">
-                            <div className="pet-date">
-                                <img src={plus} className="fa-plus" /> Дата оприлюднення: 15 травня 2023
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="pet-right">
-                    <div className="pet-stepper">
-                        <div className="pet-counts">
-                            <strong>3</strong> голоси
-                        </div>
-                        <div className="pet-progress">
-                            <div className="progress-full">
-                                <span style={{ width: '2%' }} className="progress-current"></span>
-                            </div>
-                        </div>
-                        <div className="pet-status">
-                            <img className="pet-status-icon" src={person} alt="Person Icon" />
-                            Триває збір підписів
-                        </div>
-                        <div className="pet-timer">Залишилося 93 дні</div>
-                        <div className="pet-hide">
-                            <a className="pet-btn">В ОБРАНЕ</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <Link to={"/petition/1"}>
+      <div className="pet-item">
+        <div className="pet-content">
+          <div className="pet-left">
+            <div className="pet-top">
+              <span className="pet-number">№22/192830-еп</span>
+              <span className="pet-separator">|</span>
+              <span className="pet-tag">#Без теми</span>
             </div>
+            <div className="pet-title">
+              <a className="pet-link" href="/petition/192830">
+                ТУТ МАЄ БУТИ ЗАГОЛОВОК.
+              </a>
+            </div>
+            <div className="pet-hide">
+              <div className="pet-date-container">
+                <div className="pet-date">
+                  <img src={plus} className="fa-plus" /> Дата оприлюднення: 15
+                  травня 2023
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="pet-right">
+            <div className="pet-stepper">
+              <div className="pet-counts">
+                <strong>3</strong> голоси
+              </div>
+              <div className="pet-progress">
+                <div className="progress-full">
+                  <span
+                    style={{ width: "2%" }}
+                    className="progress-current"
+                  ></span>
+                </div>
+              </div>
+              <div className="pet-status">
+                <img
+                  className="pet-status-icon"
+                  src={person}
+                  alt="Person Icon"
+                />
+                Триває збір підписів
+              </div>
+              <div className="pet-timer">Залишилося 93 дні</div>
+              <div className="pet-hide">
+                <a className="pet-btn">В ОБРАНЕ</a>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </Link>
+  );
 };
 
 export default Petition;
