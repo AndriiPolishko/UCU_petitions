42d57f0519cbc406d1344a78f6237f2f7090bfeb
Roman Naumenko <naumenko.roman2003@gmail.com>
2023-05-22 18:00:57 +0300

    fix styles

--- src/PetitionPage/PetitionPage.js
+++ src/PetitionPage/PetitionPage.js
@@ -1,46 +1,48 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import './PetitionPage.css';
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import "./PetitionPage.css";
+import Button from "../Button/Button";
+import CircleDiagram from "../CircleDiagram/CircleDiagram";
 
 function PetitionPage() {
   const { id } = useParams();
-  const testPost = fetch('https://jsonplaceholder.typicode.com/posts/1')
+  const testPost = fetch("https://jsonplaceholder.typicode.com/posts/1")
     .then((response) => response.json())
     .then((json) => console.log(json));
   const petitions = [
     {
       id: 1,
-      name: 'Sample',
-      author: 'Andrii',
-      date: '15.05.2023',
+      name: "Sample",
+      author: "Andrii",
+      date: "15.05.2023",
       longDescription: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id reprehenderit magnam, corporis laudantium delectus repellat \
         sit nobis corrupti praesentium quibusdam maxime pariatur sunt explicabo accusantium debitis necessitatibus perspiciatis \
         esse consequatur?`,
       signers: [
-        'first name second name',
-        'first name second name',
-        'first name second name',
-        'first name second name',
-        'first name second name',
+        "first name second name",
+        "first name second name",
+        "first name second name",
+        "first name second name",
+        "first name second name",
       ],
       signs: 123,
       signsNeeded: 321,
-      status: 'Очікування',
+      status: "Очікування",
     },
     {
       id: 2,
-      name: 'Sample 2',
-      author: 'Roman',
-      date: '15.05.2320',
+      name: "Sample 2",
+      author: "Roman",
+      date: "15.05.2320",
       longDescription: `Lorem ipsum dolor, sit amet consectetur adipisicing elit.`,
       signers: [
-        'first name second name',
-        'first name second name',
-        'first name second name',
+        "first name second name",
+        "first name second name",
+        "first name second name",
       ],
       signs: 3,
       signsNeeded: 100,
-      status: 'Очікування',
+      status: "Очікування",
     },
   ];
 
@@ -53,25 +55,33 @@ function PetitionPage() {
       <section className="petitionData">
         <h1>{petition.name}</h1>
         <p>Автор: {petition.author}</p>
-        <p className="date">Дата публікації:{petition.date}</p>
+        <p className="date">Дата публікації: {petition.date}</p>
         <div className="textAndSigners">
           <div className="buttonContainer">
-            <button
+            <Button
               onClick={() => {
                 setTextOrSigners(1);
               }}
-              className={textOrSigners === 1 ? 'green' : null}
+              className={
+                textOrSigners === 1
+                  ? "green span-select-left-button"
+                  : "span-select-left-button"
+              }
             >
               Текст публікації
-            </button>
-            <button
+            </Button>
+            <Button
               onClick={() => {
                 setTextOrSigners(0);
               }}
-              className={textOrSigners === 0 ? 'green' : null}
+              className={
+                textOrSigners === 0
+                  ? "green span-select-right-button"
+                  : "span-select-right-button"
+              }
             >
               Підписанти
-            </button>
+            </Button>
           </div>
           {textOrSigners === 1 ? (
             <div className="textContainer">{petition.longDescription}</div>
@@ -82,14 +92,15 @@ function PetitionPage() {
       </section>
       <section className="importantInfo">
         <div className="importantDataContainer">
+          <CircleDiagram
+            totalSigns={petition.signs}
+            neededSigns={petition.signsNeeded}
+          />
           <div className="importantDataContainer_text">
-            <p>
-              {petition.signs} / {petition.signsNeeded}
-            </p>
             <p>Статус: {petition.status}</p>
             <p>Залишилось часу: Lorem</p>
           </div>
-          <button className="button">Підписати</button>
+          <Button className="petition-sign-button">Підписати</Button>
         </div>
         <div className="petitionGist ">
           <p className="petitionGist_text">
@@ -98,7 +109,7 @@ function PetitionPage() {
             doloremque blanditi is minus, fugit delectus impe dit a saepe
             quaerat doloribus eos, voluptas voluptates excepturi?
           </p>
-          <button className="">Додати в обране</button>
+          <Button className="add-to-chosen-button">Додати в обране</Button>
         </div>
       </section>
     </div>
