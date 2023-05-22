36b938bf92500c863d3e91f02a82a519e63b19ad
rwmutel <romanmutel@gmail.com>
2023-05-22 16:44:52 +0300

    fixed data fetching

--- src/PetitionPage/PetitionPage.js
+++ src/PetitionPage/PetitionPage.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import './PetitionPage.css';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./PetitionPage.css";
 
 function PetitionPage() {
   const [petition, setPetition] = useState([]);
@@ -8,33 +8,31 @@ function PetitionPage() {
   const { id } = useParams();
 
   const fillerObj = {
-    name: 'Sample',
-    author: 'Andrii',
-    date: '15.05.2023',
+    name: "Sample",
+    author: "Andrii",
+    date: "15.05.2023",
     longDescription: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id reprehenderit magnam, corporis laudantium delectus repellat \
   sit nobis corrupti praesentium quibusdam maxime pariatur sunt explicabo accusantium debitis necessitatibus perspiciatis \
   esse consequatur?`,
     signers: [
-      'first name second name',
-      'first name second name',
-      'first name second name',
-      'first name second name',
-      'first name second name',
+      {id:0, name: "first name second name"},
+      {id:1, name: "first name second name"}, 
+      {id:2, name: "first name second name"},
+      {id:3, name: "first name second name"}
     ],
     signs: 123,
     signsNeeded: 321,
-    status: 'Очікування',
+    status: "Очікування",
   };
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
-        setPetition({...fillerObj, ...json});
+        setPetition({ ...fillerObj, ...json });
       })
       .catch((error) => {
-        console.log('Error:', error);
+        console.log("Error:", error);
       });
   }, []);
 
@@ -52,7 +50,7 @@ function PetitionPage() {
               onClick={() => {
                 setTextOrSigners(1);
               }}
-              className={textOrSigners === 1 ? 'green' : null}
+              className={textOrSigners === 1 ? "green" : null}
             >
               Текст публікації
             </button>
@@ -60,7 +58,7 @@ function PetitionPage() {
               onClick={() => {
                 setTextOrSigners(0);
               }}
-              className={textOrSigners === 0 ? 'green' : null}
+              className={textOrSigners === 0 ? "green" : null}
             >
               Підписанти
             </button>
@@ -84,9 +82,9 @@ function PetitionPage() {
           <button className="button">Підписати</button>
         </div>
         <div className="petitionGist ">
-          <p className="petitionGist_text">
-            {petition.body}
-          </p>
+          {petition.signers && petition.signers.map((user) => (
+            <li className="petitionGist_text" key={user.id}>{user.name}</li>
+          ))}
           <button className="">Додати в обране</button>
         </div>
       </section>
